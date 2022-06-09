b1c4e8aaad9d401fe328eb652a09dfca86fd1ec9
mertp <71186477+mertp@users.noreply.github.com>
2022-06-09 14:08:29 +0300

    Add files via upload

---
+++ Music.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+    [Id] INT NOT NULL PRIMARY KEY, 
+    [SongName] NVARCHAR(50) NOT NULL, 
+    [AlbumName] NCHAR(50) NOT NULL, 
+    [Artist] NCHAR(50) NOT NULL, 
+    [SongTime] NCHAR(50) NOT NULL, 
+    [ReleaseDate] INT NOT NULL
+)
